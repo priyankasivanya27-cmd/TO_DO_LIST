cc123c32bc88ce7c979d7834f1a86c42aa499e2d
priyankasivanya27-cmd <priyankasivanya27@gmail.com>
2025-09-10 15:25:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "X";
+  delBtn.className = "delete";
+  delBtn.onclick = () => li.remove();
+
+  li.appendChild(delBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
